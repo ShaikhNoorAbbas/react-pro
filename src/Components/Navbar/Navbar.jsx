1bc8cdad703bcac834f558dc1eeac803d7a80365
Noor Abbas <shaikhnoor1512@gmail.com>
2024-09-19 16:53:56 +0530

    added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,15 +3,17 @@ import "./navbar.css";
 import logo from "../../assets/logo.png";
 const Navbar = () => {
   return (
-    <nav>
-      <img src={logo} alt="Pro" />
+    <nav className="container">
+      <img src={logo} alt="Pro" className="logo" />
       <ul>
         <li>Home</li>
         <li>Program</li>
         <li>About us</li>
         <li>Campus</li>
         <li>Testimonials</li>
-        <li>Contact us</li>
+        <li>
+          <button className="btn">Contact us</button>
+        </li>
       </ul>
     </nav>
   );
