2d314032fcf808889d245839c9cc94f936e34051
Noor Abbas <shaikhnoor1512@gmail.com>
2024-09-24 10:43:33 +0530

    Working

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "./hero.css";
+import dark_arrow from "../../assets/dark-arrow.png";
 function Hero() {
-  return <div className="hero">Hero</div>;
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>we ensure better education for better world</h1>
+        <p>
+          our cutting-edge curriculum is designed to empower studends with the
+          knowledge,skills, and experience needed to excel in the dynamic field
+          of education
+        </p>
+        <button className="btn">
+          Explore more <img src={dark_arrow} alt="->" />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
